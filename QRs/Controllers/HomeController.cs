fee3178ddb81e128d1db119768a9b1bd582c0a95
Christopher Smith <csmith4work@gmail.com>
2019-01-26 17:18:26 -0600

    QR code generation is successful.

--- QRs/Controllers/HomeController.cs
+++ QRs/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QRCoder;
 using QRs.Models;
 
 namespace QRs.Controllers
@@ -17,8 +18,22 @@ namespace QRs.Controllers
 
         public IActionResult GetCode(string input)
         {
+            QRCodeGenerator qr = new QRCodeGenerator();
+            QRCodeData data = qr.CreateQrCode(input, QRCodeGenerator.ECCLevel.Q);
+            SvgQRCode qrCode = new SvgQRCode(data);
+            string qrCodeSVG = qrCode.GetGraphic(20);
+            QRString qrs = new QRString(qrCodeSVG);
 
-            return View();
+            return View(qrs);
+        }
+
+        public class QRString
+        {
+            public string QRSVG { get; set; }
+            public QRString(string qrstring)
+            {
+                QRSVG = qrstring;
+            }
         }
     }
 }
